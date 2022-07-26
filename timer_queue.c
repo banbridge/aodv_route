59ca1b4e1fc6acbb13a3b1c7df79be3228c4563e
banbridge <gyb@bupt.edu.cn>
2022-07-26 23:34:33 +0800

    "timer_queue补全"

--- timer_queue.c
+++ timer_queue.c
@@ -24,6 +24,9 @@ static void printTQ(list_t * l);
 #endif
 #endif                /* NS_PORT */
 
+/*
+ * 用来初始化一个timer（绑定处理函数等等）
+ */
 int NS_CLASS timer_init(struct timer *t, timeout_func_t f, void *data) {
 
     if (!t) {
@@ -39,6 +42,10 @@ int NS_CLASS timer_init(struct timer *t, timeout_func_t f, void *data) {
 }
 
 /* Called when a timer should timeout */
+/**
+ * //当一个timer到时的时候调用的函数，作用是把某个时间前的所有timer都移除，并且调用他们的处理函数
+ * @param now
+ */
 void NS_CLASS timer_timeout(struct timeval *now) {
     LIST(expTQ);
     list_t *pos, *tmp;
@@ -76,6 +83,10 @@ void NS_CLASS timer_timeout(struct timeval *now) {
     }
 }
 
+/**
+ * 如果已经used，首先删除，然后添加，在添加的时候遍历整个TQ，找到合适的位置插入（保证时序）
+ * @param t
+ */
 NS_STATIC void NS_CLASS timer_add(struct timer *t) {
     list_t *pos;
 
@@ -118,3 +129,132 @@ NS_STATIC void NS_CLASS timer_add(struct timer *t) {
 #endif
 
 }
+
+/**
+ * 在TQ链表中删除指定timer
+ * @param t
+ * @return
+ */
+int NS_CLASS timer_remove(struct timer *t) {
+    int res = 1;
+
+    if (!t)
+        return -1;
+
+
+    if (list_unattached(&t->l))
+        res = 0;
+    else
+        list_detach(&t->l);
+
+    t->used = 0;
+
+    return res;
+}
+
+/**
+ * 删除当前timer，然后调用处理函数（不知道这个有啥用）
+ * @param t
+ * @return
+ */
+int NS_CLASS timer_timeout_now(struct timer *t) {
+    if (timer_remove(t)) {
+
+#ifdef NS_PORT
+        (*this.*t->handler) (t->data);
+#else
+        t->handler(t->data);
+#endif
+        return 1;
+    }
+    return -1;
+}
+
+/**
+ * 改变某个timer的timeout（如果这个timer已经进入队列也就是used了，首先要移出）
+ * @param t
+ * @param msec
+ */
+void NS_CLASS timer_set_timeout(struct timer *t, long msec) {
+    if (t->used) {
+        timer_remove(t);
+    }
+
+    gettimeofday(&t->timeout, NULL);
+
+    if (msec < 0)
+            DEBUG(LOG_WARNING, 0, "Negative timeout!!!");
+
+    t->timeout.tv_usec += msec * 1000;
+    t->timeout.tv_sec += t->timeout.tv_usec / 1000000;
+    t->timeout.tv_usec = t->timeout.tv_usec % 1000000;
+
+    timer_add(t);
+}
+
+long timer_left(struct timer *t) {
+    struct timeval now;
+
+    if (!t)
+        return -1;
+
+    gettimeofday(&now, NULL);
+
+    return timeval_diff(&now, &t->timeout);
+}
+
+/**
+ * 首先把当前时间（now）之前的timer全部到时（也就是调用timer_timeout(&now)），然后返回下一个即将到期的timer剩下的时间
+ * @return
+ */
+struct timeval *NS_CLASS timer_age_queue() {
+    struct timeval now;
+    struct timer *t;
+    static struct timeval remaining;
+
+    gettimeofday(&now, NULL);
+
+    fflush(stdout);
+
+    if (list_empty(&TQ))
+        return NULL;
+
+    timer_timeout(&now);
+
+    /* Check emptyness again since the list might have been updated by a
+     * timeout */
+    if (list_empty(&TQ))
+        return NULL;
+
+    t = (struct timer *) TQ.next;
+
+    remaining.tv_usec = (t->timeout.tv_usec - now.tv_usec);
+    remaining.tv_sec = (t->timeout.tv_sec - now.tv_sec);
+
+    if (remaining.tv_usec < 0) {
+        remaining.tv_usec += 1000000;
+        remaining.tv_sec -= 1;
+    }
+    return (&remaining);
+}
+
+
+#ifdef DEBUG_TIMER_QUEUE
+void NS_CLASS printTQ(list_t * l){
+    struct timeval now;
+    int n = 0;
+    list_t *pos;
+
+    gettimeofday(&now, NULL);
+
+    fprintf(stderr, "================\n");
+    fprintf(stderr, "%-12s %-4s %lu\n", "left", "n", (unsigned long) l);
+
+    list_foreach(pos, l) {
+    struct timer *t = (struct timer *) pos;
+    fprintf(stderr, "%-12ld %-4d %lu\n", timeval_diff(&t->timeout, &now), n,
+        (unsigned long) pos);
+    n++;
+    }
+}
+#endif
--- timer_queue.h
+++ timer_queue.h
@@ -2,6 +2,14 @@
 // Created by ByteDance on 2022/7/25.
 //
 
+/*
+ * 总览整个 timer_queue 的功能，其实就是一个离散事件模拟器，
+ * 用一个链表记录即将到来的事件，然后对相应的事件调用相应的处理函数，
+ * 需要计时的比如hello消息的统计都会通过这个方式来完成。另外里面还有一
+ * 个 gettimeofday函数，用来获取当前时间，它实际上是获取了调度器
+ * Scheduler的时钟。
+ */
+
 #ifndef AODV_ROUTE_TIMER_QUEUE_H
 #define AODV_ROUTE_TIMER_QUEUE_H
 
@@ -19,11 +27,11 @@ typedef void (*timeout_func_t) (void *);
 
 
 struct timer{
-    list_t l;
-    int used;
-    struct timeval timeout;
-    timeout_func_t handler;
-    void *data;
+    list_t l; //链表域
+    int used; //主要是用来记录该timer是否已经进入队列，当我们在添加一个timer的时候，如果它已经进入了队列，首先要把它删除，然后再添加
+    struct timeval timeout; //记录时间的 timeval 变量
+    timeout_func_t handler; //记录时间到了的时候调用的处理函数指针（类型为 timeout_func_t）
+    void *data; //一个void类型的指针，存储一些handler需要的数据
 };
 
 static inline long timeval_diff(struct timeval *t1, struct timeval *t2){
@@ -52,16 +60,24 @@ static inline int timeval_add_msec(struct timeval *t, unsigned long msec){
 #endif				/* NS_NO_GLOBALS */
 
 #ifndef NS_NO_DECLARATIONS
+//未实现（不过在 timer_queue.c 里面定义了一个链表头TQ）
 void timer_queue_init();
+//在TQ链表中删除指定timer
 int timer_remove(struct timer *t);
+//改变某个timer的timeout（如果这个timer已经进入队列也就是used了，首先要移出）
 void timer_set_timeout(struct timer *t, long msec);
 int timer_timeout_now(struct timer *t);
+//首先把当前时间（now）之前的timer全部到时（也就是调用timer_timeout(&now)），然后返回下一个即将到期的timer剩下的时间
 struct timeval *timer_age_queue();
 /* timer_init should be called for every newly allocated timer */
+//用来初始化一个timer（绑定处理函数等等）
 int timer_init(struct timer *t, timeout_func_t f, void *data);
 
 #ifdef NS_PORT
+//如果已经used，首先删除，然后添加，在添加的时候遍历整个TQ，找到合适的位置插入（保证时序）
 void timer_add(struct timer *t);
+
+//当一个timer到时的时候调用的函数，作用是把某个时间前的所有timer都移除，并且调用他们的处理函数
 void timer_timeout(struct timeval *now);
 
 #ifdef DEBUG_TIMER_QUEUE
