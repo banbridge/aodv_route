d9846be3d210bd601591bba6acd139c3aeacfead
高逸博 <gaoyibo.09@bytedance.com>
2022-08-09 22:33:14 +0800

    2022-08-09 rreq

--- aodv_rreq.c
+++ aodv_rreq.c
@@ -2,5 +2,22 @@
 // Created by ByteDance on 2022/7/27.
 //
 
+#ifdef NS_PORT
+#include "ns-2/aodv-uu.h"
+#else
+#include <netinet/in.h>
+
 #include "aodv_rreq.h"
+#include "aodv_rrep.h"
+#include "routing_table.h"
+#include "aodv_timeout.h"
+#include "timer_queue.h"
+#include "aodv_socket.h"
+#include "params.h"
+#include "seek_list.h"
+#include "defs.h"
+#include "debug.h"
+
+#include "locality.h"
+#endif
 
